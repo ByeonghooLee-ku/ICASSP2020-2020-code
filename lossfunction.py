abcdb1800ed00678d74994a610571e43f731959d
ByeonghooLee-Ku <50348123+ByeonghooLee-ku@users.noreply.github.com>
2021-02-19 13:30:34 +0900

    Update lossfunction.py

--- lossfunction.py
+++ lossfunction.py
@@ -6,14 +6,14 @@ class CroppedLoss:
     def __init__(self):
         self.loss_function = nn.CrossEntropyLoss()
 
-    def __call__(self, preds, targets, probability, batchSize):
+    def __call__(self, preds, targets, predictions, batchSize):
         loss = 0
         for i in range(0, batchSize):
             outputsSlice = preds[i, :]
             outputsSlice = outputsSlice[np.newaxis, :]
             targetsSlice = targets[np.newaxis, i]
-            probabilitySlice = probability[i]
-            loss += self.loss_function(outputsSlice, targetsSlice) * probabilitySlice
+            predictionSlice = predictions[i]
+            loss += self.loss_function(outputsSlice, targetsSlice) * predictionSlice
 
         loss = loss / batchSize
         return loss
